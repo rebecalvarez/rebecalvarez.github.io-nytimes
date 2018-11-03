8bf0c01bdd6af76678d952a21de66aebcda1450e
rebecalvarez <rebeca.dodero@gmail.com>
2018-11-03 14:04:19 -0500

    Pulled down latest version of backend code, resolved merge conflict

--- assets/javascript/nyt.js
+++ assets/javascript/nyt.js
@@ -7,8 +7,45 @@ url += '?' + $.param({
 $.ajax({
   url: url,
   method: 'GET',
-}).done(function(result) {
+}).then(result => logResponse(result));
+
+function logResponse(result) {
   console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+  console.log (result.response.docs[2].keywords[0].value);
+
+}
+// $("button").on("click", function() {
+//   var person = $(this).attr("data-person");
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+//     person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//     .then(function(response) {
+//       handleResponse(response.data)
+
+      
+//     });
+// });
+
+
+// function handleResponse(results){
+//   for (var i = 0; i < results.length; i++) {
+//         var gifDiv = $("<div>");
+
+//         var rating = results[i].rating;
+
+//         var p = $("<p>").text("Rating: " + rating);
+
+//         var personImage = $("<img>");
+//         personImage.attr("src", results[i].keywords.value);
+
+//         gifDiv.prepend(p);
+//         gifDiv.prepend(personImage);
+
+//         $("#gifs-appear-here").prepend(gifDiv);
+//       }
+
+// }
